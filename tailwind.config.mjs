8ac907765b890ad9b091e2d7aad664560bfaf54b
ChortJulio <chortjulio@gmail.com>
2024-03-05 20:28:32 -0300

    Add colors to tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        yellow_primary: '#efe52a',
+        orange_primary: '#ec6317',
+        pink_primary: '#a5175b',
+        pink_secondary: '#c26ba8',
+        purple_primary: '#891f76',
+      },
+    },
+  },
+  plugins: [],
 }
